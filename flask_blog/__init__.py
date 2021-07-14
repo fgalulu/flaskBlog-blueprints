1212aa422af42df1ee1a698da6eb999860355342
fiona <fionasonny132@gmail.com>
2021-07-14 13:41:21 +0300

    blueprint error

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,15 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_blog.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ab1f25d0a160b87ad310899a2fe88d34'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from flask_blog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts
+    from flask_blog.main.routes import main
+    app.register_blueprint(users, name='users_blueprint')
+    app.register_blueprint(posts, name='posts_blueprint')
+    app.register_blueprint(main, name='main_blueprint')
+
+    return app
